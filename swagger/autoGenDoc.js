02e70c246f3f506cff92617bbb4f84322a40d4e8
Igor Saiti <saitiigor@gmail.com>
2023-07-22 14:58:42 -0300

    first commit

---
+++ swagger/autoGenDoc.js
@@ -0,0 +1,43 @@
+const mongooseToSwagger = require('mongoose-to-swagger');
+// const EsquemaTarefa = require('../src/models/tarefa.js');
+// const EsquemaUsuario = require('../src/models/usuario.js');
+const swaggerAutogen = require('swagger-autogen')({
+    openapi: '3.0.0',
+    language: 'pt-BR'
+});
+
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['../index.js', '../src/routes.js'];
+
+let doc = {
+    info: {
+        version: "1.0.0",
+        title: "API do BoardTasks",
+        description: "Documentação da API do BoardTasks"
+    },
+    servers: [
+        {
+            url: "http://localhost:4000/",
+            description: "Servidor localhost"
+        },
+        {
+            url: "http://boardtasks-back.versel.app",
+            description: "Servidor de produção"
+        }
+    ],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    // components: {
+    //     schemas: {
+    //         Usuario: mongooseToSwagger(EsquemaUsuario),
+    //         Tarefa: mongooseToSwagger(EsquemaTarefa),
+    //     }
+    // }
+}
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    console.log("Documentação do Swagger gerada encontra-se no arquivo em "+ outputFile);
+    if(process.env.NODE_ENV !== 'production'){
+        require("../index.js");
+    }
+})
