9756e1d4d848abc1a8473fbc51d8198a1b7f5456
Igor Saiti <saitiigor@gmail.com>
2023-07-24 13:46:02 -0300

    Reestruturação de pastas

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('./swagger/swagger_output.json');
 const swaggerOptions = { customCssUrl: '/swagger-ui.css'}
-
-const usersRouter = require('./routes/users');
-
+const routes = require('./src/routes');
 const app = express();
 require('dotenv').config();
 
@@ -19,9 +17,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
-app.use('/users', usersRouter);
+if(process.env.NODE_ENV !== 'test'){
+    const swaggerFile = require('./swagger/swagger_output.json')
+    app.get('/', (req, res) => { /* #swagger.ignore = true */ res.redirect('/doc'); });
+    app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));
+}
+
+routes(app);
 
 if(process.env.NODE_ENV !== 'test'){
     const PORT = process.env.PORT || 4000;
