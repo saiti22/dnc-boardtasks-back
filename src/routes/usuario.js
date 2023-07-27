979124cee96763865a46176bb944acfc13905f81
Igor Saiti <saitiigor@gmail.com>
2023-07-27 11:02:57 -0300

    Nova rota login de usuário

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -2,6 +2,7 @@ const express = require('express');
 const conectarBancoDados = require('../middlewares/conectarBD');
 const tratarErrosEsperados = require('../functions/tratarErrosEsperados');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const EsquemaUsuario = require('../models/usuario');
 const router = express.Router();
 
@@ -13,6 +14,44 @@ router.post('/criar', conectarBancoDados, async function(req, res) {
     const numeroVezesHash = 10;
     const senhaHash = await bcrypt.hash(senha, numeroVezesHash);
     const respostaBD = await EsquemaUsuario.create({nome, email, senha: senhaHash});
+
+    res.status(200).json({
+      status: "OK",
+      statusMensagem: "Usuário criado com suceso.",
+      resposta: respostaBD
+    })
+
+  } catch (error) {
+    if (String(error).includes("email_1 dup Key")) {
+      return tratarErrosEsperados(res, "Error: Já existe uma conta com este email.");
+    }
+    return tratarErrosEsperados(res, error);
+  }
+});
+
+router.post('/logar', conectarBancoDados, async function(req, res) {
+  try{
+    // #swagger.tags = ['Usuario']
+    let {email, senha} = req.body;
+    
+    let respostaBD = await EsquemaUsuario.findOne({ email }).select('+senha');
+    if (respostaBD) {
+      let senhaCorreta = await bcrypt.compare(senha, respostaBD.senha);
+      if (senhaCorreta) {
+        let token = jwt.sign({ id: respostaBD._id }, porcess.env.JWT_SECRET, {expiresIn: '1d' });
+
+        res.header('x-auth-token', token);
+        res.status(200).json({
+          status: "OK",
+          statusMensagem: "Usuário autenticado com sucesso.",
+          resposta: { "x-auth-token": token }
+        });
+      } else {
+        throw new Error("Email ou senha incorreta");
+      }
+    } else {
+      throw new Error("Email ou senha incorreta");
+    }
 
     res.status(200).json({
       status: "OK",
