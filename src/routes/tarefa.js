9a43adce8c29655ecff7eb37aa1ff5ca4f0885f7
Igor Saiti <saitiigor@gmail.com>
2023-07-27 11:52:10 -0300

    Edição de tarefa

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -24,4 +24,32 @@ router.post('/criar', authUser, conectarBancoDados, async function(req, res) {
   }
 });
 
+router.put('/editar/:id', authUser, conectarBancoDados, async function(req, res) {
+    try{
+      // #swagger.tags = ['Tarefa']
+      let idTarefa = req.params.id;
+      let {posicao, titulo, descricao, status, dataEntrega} = req.body;
+      const usuarioLogado = req.usuarioJwt.id;
+
+      const checkTarefa = await EsquemaTarefa.findOne({_id: idTarefa, criador: usuarioLogado})        ;
+      if(!checkTarefa){
+        throw new Error("Tarefa não encontrada ou pertence a outro usuário");
+      }
+
+      const tarefaAtualizada = await EsquemaTarefa.updateOne({_id: idTarefa}, {posicao, titulo, descricao, status, dataEntrega});
+      if (tarefaAtualizada?.modifiedCount > 0) {
+        const dadosTarefa = await EsquemaTarefa.findOne({_id: idTarefa}).populate('criador');
+
+        res.status(200).json({
+            status: "OK",
+            statusMensagem: "Tarefa atualizada com suceso.",
+            resposta: dadosTarefa
+          })
+      }
+  
+    } catch (error) {
+      return tratarErrosEsperados(res, error);
+    }
+  });
+
 module.exports = router;
